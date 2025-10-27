b7269dbd903f9e0898d0ef86ba3c86cf119feed7
Gelson matias <matiasoliveira372@gmail.com>
2025-10-27 20:45:16 +0000

    error on tabScreen fixed

--- routes/TabRoutes/tab.route.js
+++ routes/TabRoutes/tab.route.js
@@ -6,18 +6,14 @@ import { Dimensions } from 'react-native';
 import Login from '../../resource/views/foundBy';
 import User from '../../resource/views/user';
 import { useState } from 'react';
-
+import SingIn from '../../resource/views/signIn';
 
 
 const Tab =createBottomTabNavigator();
 const { width } = Dimensions.get('window');
 const size = width * 0.8; // 20% da largura da tela
 const size01 = width * 0.1; // 20% da largura da tela
-<<<<<<< HEAD
-export default function TabRouter({ setLogin }) {
-=======
 export default function TabRouter({ setLogin,login }) {
->>>>>>> DEV-02
   const[sing, setSing]=useState(true)
   const closeAccount=()=>{
       console.log(123);
@@ -110,12 +106,6 @@ export default function TabRouter({ setLogin,login }) {
    
       <Tab.Screen
         name="singIn"
-<<<<<<< HEAD
-        component={() => null} // não mostra tela
-        options={{
-          tabBarIcon: ({ color }) => (
-            <FontAwesome name="sign-out" size={35} color={color} />
-=======
         component={SingIn} // não mostra tela
         options={{
           tabBarIcon: ({ color }) => (
@@ -124,7 +114,6 @@ export default function TabRouter({ setLogin,login }) {
                 (<FontAwesome name="sign-in" size={35} color={color} />)
             
            
->>>>>>> DEV-02
           ),
           tabBarShowLabel: false,
           headerShown: false,
@@ -132,11 +121,7 @@ export default function TabRouter({ setLogin,login }) {
         listeners={{
           tabPress: e => {
             e.preventDefault(); // evita navegação
-<<<<<<< HEAD
-            setLogin(false); // faz logout e volta pro Stack
-=======
             setLogin("SingIn"); // faz logout e volta pro Stack
->>>>>>> DEV-02
           },
         }}
       />
